47a3db877c8283609a1df99674705b5b7b48df7c
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-04 20:18:40 +0200

    Solve exercise

--- src/App.js
+++ src/App.js
@@ -3,59 +3,89 @@ import fav from "./fav.svg";
 import settings from "./settings.svg";
 import profile from "./profile.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("HOME");
+
+  function handleTitleChange(name) {
+    setTitle(name);
+  }
+
+  function handleNavBarClick(name) {
+    setTitle(name);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <NavBar />
-      <Input />
+      <Header title={title} />
+      <NavBar handleNavBarClick={handleNavBarClick} />
+      <Input handleTitleChange={handleTitleChange} />
     </div>
   );
 }
 
 export default App;
 
-function Header() {
-  return <header>Title</header>;
+function Header({ title }) {
+  return <header>{title}</header>;
 }
 
-function NavBar() {
+function NavBar({ handleNavBarClick }) {
+  function handleHomeClick() {
+    handleNavBarClick("HOME");
+  }
+  function handleFavoritesClick() {
+    handleNavBarClick("FAVORITES");
+  }
+  function handleSettingsClick() {
+    handleNavBarClick("SETTINGS");
+  }
+  function handleProfileClick() {
+    handleNavBarClick("PROFILE");
+  }
+
   return (
     <div className="NavBar">
-      <button>
+      <button onClick={handleHomeClick}>
         <img src={home} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleFavoritesClick}>
         <img src={fav} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleSettingsClick}>
         <img src={settings} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleProfileClick}>
         <img src={profile} className="App-logo" alt="logo" />
       </button>
     </div>
   );
 }
 
-function Input() {
-  return <AnotherDiv />;
+function Input({ handleTitleChange }) {
+  return <AnotherDiv handleTitleChange={handleTitleChange} />;
 }
 
-function AnotherDiv() {
-  return <YouGottaBeKiddingMeDiv />;
+function AnotherDiv({ handleTitleChange }) {
+  return <YouGottaBeKiddingMeDiv handleTitleChange={handleTitleChange} />;
 }
 
-function YouGottaBeKiddingMeDiv() {
+function YouGottaBeKiddingMeDiv({ handleTitleChange }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formInput = form.name.value.toUpperCase();
+    handleTitleChange(formInput);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         id="name"
         name="name"
         placeholder="Type here any title you like"
       ></input>
-      <button>Change header title from input</button>
+      <button type="submit">Change header title from input</button>
     </form>
   );
 }
